e617c7443641311eab375784358b97d8f44e52d5
ivanzapatarivera <falenyo@gmail.com>
2020-09-20 15:25:28 -0400

    modifying search results cards

--- employee-directory/src/components/SearchResults/index.js
+++ employee-directory/src/components/SearchResults/index.js
@@ -10,39 +10,35 @@ function SearchResults(props) {
 
   return (
 
-    // Main div
-    <div className="">
+  // Main div
+  <div className="">
 
       {/* Galaxy Team */}
       <div className="row">
         <div className="col-sm-12">
-
           <h1 className="text-center my-5 galaxyfont">{props.galaxy}</h1>
-
-        </div>
-        
-            
-       
-
+        </div>        
       </div>
       
       {props.result.map(result => (
-          
+
+        <div>
+          {/* Location of image */}
           <div className="m-2 p-1 row ">
-            
-            {/* Location of image */}
-            <div className="col-sm-2">
+            <div className="col-sm-12">
               <img alt="Person" src={result.picture.large} className="img-fluid" />
             </div>
-
+          </div>
+            
+                     
+           
             {/* Location of employee info */}
-            <div className="col-sm-6">
+          <div className="m-2 p-1 row ">
+            <div className="col-sm-12">
               <p class="employeeName">{result.name.first} {result.name.last} | Galactic Team: {props.galaxy}</p>
-              
             </div>
-
           </div>      
-
+        </div>
       ))}
 
     </div>
@@ -51,3 +47,18 @@ function SearchResults(props) {
 }
 
 export default SearchResults;
+
+
+{/* <div className="col-sm-12">
+
+<div className="row">
+<div className="card" style="width: 18rem;">
+    <img src={props.result.picture.large} className="card-img-top" alt={props.result.name.first + " " + props.result.name.last} />
+    <div classNameName="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</div>
+</div> */}
