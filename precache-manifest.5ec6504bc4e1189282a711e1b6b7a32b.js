a200f837ebb49be15df4a5c73fd6b6f5725ca12f
ivanzapatarivera <falenyo@gmail.com>
2020-09-20 18:09:28 -0400

    Updates

---
+++ precache-manifest.5ec6504bc4e1189282a711e1b6b7a32b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43b59f6d83e09603cf54ca14abef6f3",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "2fda8a70a69c601ba71b",
+    "url": "/employee-directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "be3c592e370895c4eb7e",
+    "url": "/employee-directory/static/css/main.7ab80b41.chunk.css"
+  },
+  {
+    "revision": "2fda8a70a69c601ba71b",
+    "url": "/employee-directory/static/js/2.a38205c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/employee-directory/static/js/2.a38205c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be3c592e370895c4eb7e",
+    "url": "/employee-directory/static/js/main.734e0f3f.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
