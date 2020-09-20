ae2c0d497018192856651c285cba009a306e75f8
ivanzapatarivera <falenyo@gmail.com>
2020-09-20 11:58:12 -0400

    changed names in list for galaxy names

--- employee-directory/src/components/SearchForm/index.js
+++ employee-directory/src/components/SearchForm/index.js
@@ -10,23 +10,23 @@ function SearchForm(props) {
         <input value={props.search} onChange={props.handleInputChange} name="nat" type="text" list="countries" id="nat" className="searchTeam form-control" placeholder="Search Your Intergalactic Team..." />
         <datalist id="countries">
          
-              <option value="AU" key="AU">Australia</option>
-              <option value="BR" key="BR">Brazil</option>
-              <option value="CA" key="CA">Canada</option>
-              <option value="CH" key="CH">China</option>
-              <option value="DE" key="DE">Germany</option>
-              <option value="DK" key="DK">Denmark</option>
-              <option value="ES" key="ES">Spain</option>
-              <option value="FI" key="FI">Finland</option>
-              <option value="FR" key="FR">France</option>
-              <option value="GB" key="GB">Great Britain</option>
-              <option value="IR" key="IR">Iran</option>
-              <option value="IE" key="IE">Ireland</option>
-              <option value="NO" key="NO">Norway</option>
-              <option value="NL" key="NL">Netherlands</option>
-              <option value="NZ" key="NZ">New Zealand</option>
-              <option value="TR" key="TR">Turkey</option>
-              <option value="US" key="US">United States</option>
+              <option value="AU" key="AU">Andromeda</option>
+              <option value="BR" key="BR">Black Eye</option>
+              <option value="CA" key="CA">Cartwheel</option>
+              <option value="CH" key="CH">Cosmos</option>
+              <option value="DE" key="DE">Eye of Sauron</option>
+              <option value="DK" key="DK">Fireworks</option>
+              <option value="ES" key="ES">Hockey Stick</option>
+              <option value="FI" key="FI">Large Magellanic</option>
+              <option value="FR" key="FR">Large Sombrero</option>
+              <option value="GB" key="GB">Medusa Merger</option>
+              <option value="IR" key="IR">Milky Way</option>
+              <option value="IE" key="IE">Milky Way</option>
+              <option value="NO" key="NO">Pinwheel</option>
+              <option value="NL" key="NL">Sculptor</option>
+              <option value="NZ" key="NZ">Southern Pinwheel</option>
+              <option value="TR" key="TR">Whirlpool</option>
+              <option value="US" key="US">Wolf-Lundmark-Melotte</option>
         </datalist>
         <button type="submit" onClick={props.handleFormSubmit} className="btn btn-dark mt-2">
           Let the Journey Begin
