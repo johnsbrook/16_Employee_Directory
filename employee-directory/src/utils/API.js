3bfb410c32fa9f4c68f685cee78313c76ecdfb6d
Ivan J. Zapata-Rivera <59859202+johnsbrook@users.noreply.github.com>
2020-10-29 22:14:44 -0400

    Update API.js
    Deleted commented code

--- employee-directory/src/utils/API.js
+++ employee-directory/src/utils/API.js
@@ -6,9 +6,5 @@ export default {
 
   getEmployeesByTeam: function(nat) {
     return axios.get("https://randomuser.me/api/?results=25&nat=" + nat)
-  },
-  // Commented as it's not required
-  // getEmployeeListByNat: function() {
-  //       return axios.get("https://randomuser.me/api/?results=20&inc=nat");
-  // }
+  }
 };
